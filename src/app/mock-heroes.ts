92c07895987fb6e193b40b9e5e2c6a6691493331
Ashil KP <ashil.kp@geodis.com>
2025-07-25 16:24:17 +0530

    Learning Angular

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,11 @@
+import {Hero} from './hero';
+
+export const HEROES: Hero[] = [
+    {id: 1, name: 'Superman'},
+    {id: 2, name: 'Batman'},
+    {id: 3,name: 'Wonder Woman'},
+    {id: 4, name: 'Flash'},
+    {id: 5, name: 'Green Lantern'},
+    {id: 6, name: 'Aquaman'},
+    {id: 7, name: 'Cyborg'}
+];
